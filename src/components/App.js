6581d0b827ac9aa54826f31550302f4bd1417fee
MaitaneAbad <maitaneabadconde@gmail.com>
2021-10-01 18:47:08 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import '../styles/App.scss';
+
+// Fichero src/components/App.js
+
+// import { Link, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      {/* <Route path='/contacto'></Route>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'></Link>
+          </li>
+          <li>
+            <Link to='/contacto'></Link>
+          </li>
+        </ul>
+      </nav> */}
+    </div>
+  );
+};
+
+export default App;
