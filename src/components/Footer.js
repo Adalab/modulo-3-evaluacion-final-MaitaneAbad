71f15290c98d2ff8de841a95492f4b49a9b50c55
MaitaneAbad <maitaneabadconde@gmail.com>
2021-10-04 19:34:42 +0200

    add fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,22 @@
 import '../styles/layout/footer.scss';
 const Footer = () => {
-  return <footer className='footer'> &copy; Maitane Abad Conde </footer>;
+  return (
+    <>
+      <footer className='footer'>
+        <p className='name'> &copy; Maitane Abad Conde </p>
+        <p className='subtitle'>Evaluación Final React.js</p>
+        <a
+          className='link'
+          target='_blank'
+          rel='noreferrer'
+          href='https://github.com/Adalab/modulo-3-evaluacion-final-MaitaneAbad'
+        >
+          {' '}
+          Github <i className='fab fa-github-alt'></i>
+        </a>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
