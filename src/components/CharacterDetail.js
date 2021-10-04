46ed3bd5e7355bd98058d850d9d78d169e659425
MaitaneAbad <maitaneabadconde@gmail.com>
2021-10-04 14:45:24 +0200

    add style detail card

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,29 +1,31 @@
 import { Link } from 'react-router-dom';
-
+import '../styles/layout/characterDetail.scss';
 import PageNotFound from './PageNotFound';
 
 const CharacterDetail = (props) => {
   console.log(props);
   if (props.character !== undefined) {
     return (
-      <div>
-        <Link to='/' className='characterDetail__link'>
-          <p>Volver atrás</p>
+      <div class='detail'>
+        <Link to='/' className='characterDetail'>
+          <p className='paragraphDetail'>Volver atrás</p>
         </Link>
-        <section>
+        <section className='sectionDetail'>
           <img
-            className='imgApi'
+            className='imgApiDetail'
             src={props.character.img}
             alt={`Foto de: ${props.character.name}`}
             title={`Foto de: ${props.character.name}`}
           />
           <div>
-            <h2 className='h2'>{props.character.name}</h2>
-            <ul>
-              <li>{props.character.species}</li>
-              <li>¿Vivo o muerto?: {props.character.status}</li>
-              <li>Planeta: {props.character.planet}</li>
-              <li>Episodios: {props.character.episode}</li>
+            <h2 className='nameDetail'>{props.character.name}</h2>
+            <ul className='listDetail'>
+              <li className='liDetail'>{props.character.species}</li>
+              <li className='liDetail'>
+                ¿Vivo o muerto?: {props.character.status}
+              </li>
+              <li className='liDetail'>Planeta: {props.character.planet}</li>
+              <li className='liDetail'>Episodios: {props.character.episode}</li>
             </ul>
           </div>
         </section>
