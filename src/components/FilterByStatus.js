e17015ed28364fd54787262eafba35e787b1750e
MaitaneAbad <maitaneabadconde@gmail.com>
2021-10-12 14:46:48 +0200

    change style filter Status

--- src/components/FilterByStatus.js
+++ src/components/FilterByStatus.js
@@ -1,38 +1,43 @@
 const FilterBystatus = (props) => {
   return (
-    <>
-      <label> Estado </label>
-      <label htmlFor='Alive'>
-        Vivo
-        <input
-          type='radio'
-          id='Alive'
-          name='status'
-          value='Alive'
-          onChange={props.handleSearchStatus}
-        />
-      </label>
-      <label htmlFor='Dead'>
-        Muerto
-        <input
-          type='radio'
-          id='Dead'
-          name='status'
-          value='Dead'
-          onChange={props.handleSearchStatus}
-        />
-      </label>
-      <label htmlFor='unknown'>
-        Desconocido
-        <input
-          type='radio'
-          id='unknown'
-          name='status'
-          value='unknown'
-          onChange={props.handleSearchStatus}
-        />
-      </label>
-    </>
+    <div className='divStatus'>
+      <label className='labelName'> Estado: </label>
+      <div className='divLabelInput'>
+        <label htmlFor='Alive' className='labelStatus'>
+          Vivo
+          <input
+            className='inputStatus'
+            type='radio'
+            id='Alive'
+            name='status'
+            value='Alive'
+            onChange={props.handleSearchStatus}
+          />
+        </label>
+        <label htmlFor='Dead' className='labelStatus'>
+          Muerto
+          <input
+            className='inputStatus'
+            type='radio'
+            id='Dead'
+            name='status'
+            value='Dead'
+            onChange={props.handleSearchStatus}
+          />
+        </label>
+        <label htmlFor='unknown' className='labelStatus'>
+          Desconocido
+          <input
+            className='inputStatus'
+            type='radio'
+            id='unknown'
+            name='status'
+            value='unknown'
+            onChange={props.handleSearchStatus}
+          />
+        </label>
+      </div>
+    </div>
   );
 };
 export default FilterBystatus;
